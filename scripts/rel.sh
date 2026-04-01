5410320737aa22ff1ef31395c9b4d14c51647873
Dimitar Yurukov <mscalindt@protonmail.com>
2026-04-01 07:35:32 +0200

    rel.sh: Mark `devices` tag as a core change
    Signed-off-by: Dimitar Yurukov <mscalindt@protonmail.com>

--- scripts/rel.sh
+++ scripts/rel.sh
@@ -24,7 +24,7 @@ SRC_LOG=$(
                 *': [doc]'*)
                     continue
                 ;;
-                *'conf: '* | *'(): '* | *'.gitmodules: '*)
+                *'conf: '* | *'(): '* | *'.gitmodules: '* | *'devices: '*)
                     printf "%s\n" "$LINE"
                 ;;
                 *)
