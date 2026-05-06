9f28c132744e8fa77afc484ad0e85f5187e34b2d
Dimitar Yurukov <mscalindt@protonmail.com>
2026-05-06 07:04:32 +0200

    conf: Print config used when building project
    Signed-off-by: Dimitar Yurukov <mscalindt@protonmail.com>

--- scripts/build_ts.sh
+++ scripts/build_ts.sh
@@ -2,6 +2,7 @@ set -e
 
 { read -r CONF < "$1"; } 2> /dev/null || CONF=
 if [ "$CONF" != "$2" ]; then
+    printf "%s\n" ">>> Using $2"
     printf "%s\n" "$2" > "$1"
 else
     printf "%s\n" "Nothing to be done for ./conf ($2)"
